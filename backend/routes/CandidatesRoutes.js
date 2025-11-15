919352d8aa439800358cefb02b678c638c79e77c
CarlosSmariano <sanchesmarianocarlos@gmail.com>
2025-11-15 14:17:36 -0300

    feat(backend): create routes

---
+++ backend/routes/CandidatesRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import CandidatesController from '../controller/CandidatesController'
+
+const controller = new CandidatesController()
+const router = express.Router()
+
+router.get("/", controller.listAll)
+router.get("/topics", controller.listTopics)
+router.get("/filter", controller.filterCandidates)
